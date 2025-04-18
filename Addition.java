30177e93772d272b820cef07d27e949977421dc3
vedants27 <vedantss27@gmail.com>
2025-04-18 19:06:54 +0530

    Added  method to perform addition of two numbers.

--- Addition.java
+++ Addition.java
@@ -2,4 +2,16 @@
 import java.util.Scanner;
 
 public class Addition { 
+     // Method to perform addition of two numbers
+    public static void performAddition(Scanner sc) {
+        try {
+            System.out.print("Enter first number: ");
+            double a = sc.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double b = sc.nextDouble();
+
+            double result = a + b;
+            System.out.println("Result = " + result);
+
  
