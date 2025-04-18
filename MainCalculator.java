68aa794a0828ad8b3110b3f94dbb25178d051933
vedants27 <vedantss27@gmail.com>
2025-04-18 18:44:58 +0530

    Added main calculator class and scanner input setup.

--- MainCalculator.java
+++ MainCalculator.java
@@ -1,1 +1,13 @@
+// Name: Vedant Shitole
+// PRN: 23070126143
+// Batch: AIML B3
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner object to read user input
+        int choice;
 
