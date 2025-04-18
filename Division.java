ad254d8a7b734237e0cf97d4015c39a5817731d4
vedants27 <vedantss27@gmail.com>
2025-04-18 19:20:15 +0530

    Added method to divide two numbers with input.

--- Division.java
+++ Division.java
@@ -3,4 +3,13 @@
 import java.util.Scanner;
 
 public class Division {
+      // Method to perform division of two numbers with divide-by-zero check
+    public static void performDivision(Scanner sc) {
+        try {
+            System.out.print("Enter numerator: ");
+            double a = sc.nextDouble();
+
+            System.out.print("Enter denominator: ");
+            double b = sc.nextDouble();
+
 
