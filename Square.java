5499537b106a21cadfedb9e955ef2e2972327324
vedants27 <vedantss27@gmail.com>
2025-04-18 19:29:50 +0530

    Added error handling for square input.

--- Square.java
+++ Square.java
@@ -11,3 +11,9 @@ public class Square {
 
             double result = a * a;
             System.out.println("Square = " + result);
+                  } catch (Exception e) {
+            System.out.println("Error in calculating square: " + e.getMessage());
+        }
+    }
+}
+
