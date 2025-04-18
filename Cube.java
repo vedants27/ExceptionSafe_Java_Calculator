7fc4180d03046a0bdd1b7a237d314e7626bb76b3
vedants27 <vedantss27@gmail.com>
2025-04-18 19:25:04 +0530

    Added method to calculate cube of a number.

--- Cube.java
+++ Cube.java
@@ -3,4 +3,12 @@
 import java.util.Scanner;
 
 public class Cube {
+    // Method to calculate the cube of a number
+    public static void performCube(Scanner sc) {
+        try {
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+
+            double result = a * a * a;
+            System.out.println("Cube = " + result);
 
