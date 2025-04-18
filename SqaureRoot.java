378b24556858c5d98cdff09a604446932c93129f
vedants27 <vedantss27@gmail.com>
2025-04-18 19:33:29 +0530

    Created method to compute square root of number.

--- SqaureRoot.java
+++ SqaureRoot.java
@@ -3,3 +3,17 @@
 import java.util.Scanner;
 
 public class SquareRoot {
+      // Method to calculate square root with negative number check
+    public static void performSquareRoot(Scanner sc) {
+        try {
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+
+            // Cannot calculate square root of negative number
+            if (a < 0) {
+                throw new ArithmeticException("Cannot compute square root of a negative number.");
+            }
+
+            double result = Math.sqrt(a);
+            System.out.println("Square Root = " + result);
+
