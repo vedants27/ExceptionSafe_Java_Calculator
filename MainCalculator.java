c1eeb8326b6493d61c1cae51f45a044323fffb95
vedants27 <vedantss27@gmail.com>
2025-04-18 18:45:37 +0530

    Added main menu display and input handler.

--- MainCalculator.java
+++ MainCalculator.java
@@ -11,3 +11,22 @@ public class MainCalculator {
         Scanner sc = new Scanner(System.in); // Scanner object to read user input
         int choice;
 
+                // Menu loop: keep running until user selects Exit
+        do {
+            // Displaying menu options
+            System.out.println("\n--- Calculator Menu ---");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Square");
+            System.out.println("6. Cube");
+            System.out.println("7. Square Root");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+
+            try {
+                // Reading user's choice
+                choice = sc.nextInt();
+
+
