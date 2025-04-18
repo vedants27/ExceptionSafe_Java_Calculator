28b926191f0479efecfb88e79e16d88bdb4bdec2
vedants27 <vedantss27@gmail.com>
2025-04-18 19:13:30 +0530

    Added method to perform subtraction of two numbers.

--- Subtraction.java
+++ Subtraction.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
 
 public class Subtraction {
+    // Method to perform subtraction of two numbers
+    public static void performSubtraction(Scanner sc) {
+        try {
+            System.out.print("Enter first number: ");
+            double a = sc.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double b = sc.nextDouble();
+
+            double result = a - b;
+            System.out.println("Result = " + result);
 
