6c371a8c4613bdc30f108ee1f3fee0d6dd921b79
vedants27 <vedantss27@gmail.com>
2025-04-18 19:29:16 +0530

    Added method to calculate square of a number.

--- Square.java
+++ Square.java
@@ -3,3 +3,11 @@
 import java.util.Scanner;
 
 public class Square {
+    // Method to calculate the square of a number
+    public static void performSquare(Scanner sc) {
+        try {
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+
+            double result = a * a;
+            System.out.println("Square = " + result);
