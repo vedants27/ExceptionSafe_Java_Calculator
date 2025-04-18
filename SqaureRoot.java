083ff2f3efc84541b7009134e6930c4bacf40cf5
vedants27 <vedantss27@gmail.com>
2025-04-18 19:33:52 +0530

    Added exception handling for square root input.

--- SqaureRoot.java
+++ SqaureRoot.java
@@ -16,4 +16,12 @@ public class SquareRoot {
 
             double result = Math.sqrt(a);
             System.out.println("Square Root = " + result);
+                      } catch (ArithmeticException e) {
+            System.out.println("Math error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error in calculating square root: " + e.getMessage());
+        }
+    }
+}
+
 
