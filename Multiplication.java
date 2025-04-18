dae7f2c8d3f79e9625dc97ffd7d6eac00afa626d
vedants27 <vedantss27@gmail.com>
2025-04-18 19:17:27 +0530

    Added method to multiply two numbers.

--- Multiplication.java
+++ Multiplication.java
@@ -2,3 +2,14 @@
 import java.util.Scanner;
 
 public class Multiplication {
+    // Method to perform multiplication of two numbers
+    public static void performMultiplication(Scanner sc) {
+        try {
+            System.out.print("Enter first number: ");
+            double a = sc.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double b = sc.nextDouble();
+
+            double result = a * b;
+            System.out.println("Result = " + result);
